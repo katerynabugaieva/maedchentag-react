d96926b9c3890bd092996a991c1446104e8ca3fe
Kateryna Bugaieva <sonnensmile@gmail.com>
2022-04-25 21:10:14 +0200

    add switcher

--- src/App.js
+++ src/App.js
@@ -6,16 +6,40 @@ import Tiktok from './Tiktok';
 import RMV from './RMV'
 import Technical from './Technical';
 import Insta from './Insta';
+import { useState } from 'react';
 
 function App() {
+  const [facebook, setFacebook] = useState(false);
+  const [tiktok, setTiktok] = useState(false);
+  const [rmv, setRmv] = useState(false);
+  const [howitwas, setHowitwas] = useState(false);
+  const [howitis, setHowitis] = useState(false);
+  const handleCheckboxChange = (event) => {
+    if(event.target.value ==='facebook') setFacebook(!facebook)
+    if(event.target.value ==='tiktok') setTiktok(!tiktok)
+    if(event.target.value ==='rmv') setRmv(!rmv)
+    if(event.target.value ==='howitwas') setHowitwas(!howitwas)
+    if(event.target.value ==='howitis') setHowitis(!howitis)
+  };
+
   return (
     <div className="App">
-      <Facebook />
-      <Tiktok /> 
-      <Technical />
+      <div>
+        <input type="checkbox" id="facebook" name="facebook" value="facebook" onChange={(event)=>handleCheckboxChange(event)}/>Facebook
+        <input type="checkbox" id="ticktok" name="ticktok" value="tiktok" onChange={handleCheckboxChange}/>TikTok
+        <input type="checkbox" id="RMV" name="RMV" value="rmv" onChange={handleCheckboxChange}/>RMV
+        <input type="checkbox" id="Howitwas" name="Howitwas" value="howitwas" onChange={handleCheckboxChange}/>How it was
+        <input type="checkbox" id="Howitis" name="Howitis" value="howitis" onChange={handleCheckboxChange}/>How it is
+
+      </div>
+      <div className='container'>
+      {facebook && <Facebook />}
+      {tiktok && <Tiktok /> }
+      {rmv && <Technical />}
      
-      <RMV />
-      <Insta/>
+      {howitwas && <RMV />}
+      {howitis &&  <Insta/>}
+      </div>
     </div>
   );
 }
